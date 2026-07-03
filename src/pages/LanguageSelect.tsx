6c870741cef95eca06407bd6d8c9498974910199
Rami <138556019+rami242424@users.noreply.github.com>
2026-07-03 14:21:00 +0900

    app & LS settings

--- src/pages/LanguageSelect.tsx
+++ src/pages/LanguageSelect.tsx
@@ -1,6 +1,17 @@
-function LanguageSelect(){
+import type { Screen } from "../App";
+
+interface ILanguageSelect {
+    setScreen : (screen: Screen) => void;
+}
+
+function LanguageSelect({setScreen}:ILanguageSelect){
     return(
-        <></>
+        <>
+            <h2>LanguageSelect</h2>
+            <button onClick={() => setScreen("phrases")}>영어</button>
+            <button onClick={() => setScreen("phrases")}>중국어</button>
+            <button onClick={() => setScreen("phrases")}>일본어</button>
+        </>
     );
 }
 
