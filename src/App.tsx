bb6c328d5946bae106e294d6b71d8bdce423dc70
Rami <138556019+rami242424@users.noreply.github.com>
2026-07-06 14:32:03 +0900

    app settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
+export type Screen = "lang" | "phrases" | "display" | "input" | "map";
+
 function App(){
+  const [screen , setScreen] = useState<Screen>("lang");
   return(
-    <></>
+    <>
+      <h2>App</h2>
+
+    </>
   );
 }
 
