d259928b875e420d7a9ae0640bd6737216d99072
Rami <138556019+rami242424@users.noreply.github.com>
2026-07-08 21:22:01 +0900

    feat: 카테고리별 문구 목록 다국어 렌더링

--- src/pages/LanguageSelect.tsx
+++ src/pages/LanguageSelect.tsx
@@ -7,7 +7,7 @@ function LanguageSelect({nextPageWithLang}:ILanguageSelectProps){
     return(
         <>
             <h5>LanguageSelect</h5>
-            {/* <button onClick={() => 언어가 영어로 된다음에 phrasehome으로 가야함}>영어</button> */}
+            <button onClick={() => nextPageWithLang("en")}>영어</button>
             <button onClick={() => nextPageWithLang("zh-Hans")}>중어</button>
             <button onClick={() => nextPageWithLang("ja")}>일어</button>
         </>
--- src/pages/PhraseHome.tsx
+++ src/pages/PhraseHome.tsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
+import type { Langs } from "../App";
+import phrases from "../data/phrases.json";
+
 interface IPhraseHomeProps {
-    language : string | null;
+    language : Langs;
 }
+type Category = "payment" | "tax-refund" | "exchange-carryIn" | "stock" | "recommendation" | "etc";
 function PhraseHome({language}:IPhraseHomeProps){
+    const [category, setCategory] = useState<Category>("payment");
+    if(language === null) return;
     return(
         <>
-            <h2>선택한언어:{language}</h2>
+            <div>
+                <h2>선택한언어:{language} 현재카테고리:</h2>
+                <button onClick={() => setCategory("payment")}>결제</button>
+                <button onClick={() => setCategory("tax-refund")}>텍스리펀</button>
+                <button onClick={() => setCategory("exchange-carryIn")}>교환불/기내반입</button>
+                <button onClick={() => setCategory("stock")}>상품위치/재고</button>
+                <button onClick={() => setCategory("recommendation")}>제품추천</button>
+                <button onClick={() => setCategory("etc")}>매장이용안내/기타</button>
+            </div>
+            <div>{category}</div>
+            <div>
+                {
+                    phrases[category].map((data) => (
+                        <button key={data.id}>
+                            <h3>{data.translations[language]}</h3>
+                            <p>{data.kr}</p>
+                        </button>
+                    ))
+                }
+            </div>
         </>
     );
 }
