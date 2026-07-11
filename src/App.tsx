c11ea96eaabcf7b11dfae6f16e3508acd9ef22c5
Rami <138556019+rami242424@users.noreply.github.com>
2026-07-11 12:00:00 +0900

    custom dip

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import { useState } from "react";
 import LanguageSelect from "./pages/LanguageSelect";
 import PhraseHome from "./pages/PhraseHome";
+import CustomerDisplay from "./pages/CustomerDisplay";
 
 
 export type Langs = "en" | "zh-Hans" | "ja" | null;
 export type Screen = "lang" | "phrases" | "display" | "input" | "map";
+export interface Phrase {
+  id: string;
+  kr: string;
+  translations: {
+    en: string,
+    "zh-Hans": string,
+    ja: string,
+  }
+}
 
 function App(){
   const [language, setLanguage] = useState<Langs>(null);
   const [screen, setScreen] = useState<Screen>("lang");
+  const [phraseState, setPhraseState] = useState<Phrase>();
   const nextPageWithLangs = (lang:Langs) => {
     setLanguage(lang);
     setScreen("phrases");
+  }
+  const nextToCustomerDisplay = (phrase:Phrase) => {
+    setPhraseState(phrase);
+    setScreen("display");
   }
   return(
     <>
   
       {screen === "lang" && <LanguageSelect nextPageWithLangs={nextPageWithLangs}/>}
       {screen === "phrases" && <PhraseHome language={language} />}
+      {screen === "display" && <CustomerDisplay nextToCustomerDisplay={nextToCustomerDisplay} language={language}/>}
     </>
   );
 }
--- src/pages/CustomerDisplay.tsx
+++ src/pages/CustomerDisplay.tsx
@@ -1,6 +1,16 @@
-function CustomerDisplay(){
+import type { Langs, Phrase } from "../App";
+import phrases from "../data/phrases.json";
+
+interface ICustomerDisplayProps {
+    language: Langs;
+    selectedPhrase: Phrase;
+}
+function CustomerDisplay({selectedPhrase, language}:ICustomerDisplayProps){
     return(
-        <></>
+        <>
+            <h2>선택된 질문 : {selectedPhrase.translations}</h2>
+            <p>{selectedPhrase.kr}</p>
+        </>
     );
 }
 
